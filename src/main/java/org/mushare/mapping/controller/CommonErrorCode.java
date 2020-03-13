dd6eed1e1b8becd05a6f8673ae5124b845f810b6
Meng Li <lm2343635@126.com>
2020-03-13 21:10:58 +0900

    Add JWT token format error

--- src/main/java/org/mushare/mapping/controller/CommonErrorCode.java
+++ src/main/java/org/mushare/mapping/controller/CommonErrorCode.java
@@ -6,7 +6,8 @@ public enum CommonErrorCode implements ErrorCode {
     AuthToken(801, "Auth token error."),
     NoPrivilge(802, "No privilege to invoke this method."),
     ObjecId(803, "Object cannot be found by the object id."),
-    SavingObject(804, "Internal error saving object.");
+    SavingObject(804, "Internal error saving object."),
+    JwtTokenFormatError(805, "JWT token formar wrong, it should be like `header.payload.sign`.");
 
     public int code;
     public String message;
