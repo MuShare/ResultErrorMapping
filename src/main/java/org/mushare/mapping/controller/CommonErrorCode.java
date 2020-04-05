7ad23329d12a819cd7f12554f717fc464139bd2a
Meng Li <lm2343635@126.com>
2020-04-05 18:14:58 +0900

    Fix type for `NoPrivilege`

--- src/main/java/org/mushare/mapping/controller/CommonErrorCode.java
+++ src/main/java/org/mushare/mapping/controller/CommonErrorCode.java
@@ -4,7 +4,7 @@ public enum CommonErrorCode implements ErrorCode {
 
     Unknown(800, "Unknown error."),
     AuthToken(801, "Auth token error."),
-    NoPrivilge(802, "No privilege to invoke this method."),
+    NoPrivilege(802, "No privilege to invoke this method."),
     ObjecId(803, "Object cannot be found by the object id."),
     SavingObject(804, "Internal error saving object."),
     JwtTokenFormatError(805, "JWT token formar wrong, it should be like `header.payload.sign`.");
